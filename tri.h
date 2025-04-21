5417a3a490ec9fc91a74f9de68c207910db2c1de
Loic Petiot <118407447+balbozorre@users.noreply.github.com>
2025-04-21 22:11:59 +0200

    idk

---
+++ tri.h
@@ -0,0 +1,10 @@
+#ifndef TRI_H_INCLUDED
+#define TRI_H_INCLUDED
+
+#include "types.h"
+#include "ligneBus.h"
+
+bool cout_node1InferieurNode2(Tstation *node1, Tstation *node2);
+bool date_node1InferieurNode2(Tstation *node1, Tstation *node2);
+
+#endif // TRI_H_INCLUDED
