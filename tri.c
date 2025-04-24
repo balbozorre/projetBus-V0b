131717c250c61d19a4c0cf9269e51b57929fb950
Loic Petiot <118407447+balbozorre@users.noreply.github.com>
2025-04-24 18:58:17 +0200

    fonction swap

--- tri.c
+++ tri.c
@@ -1,2 +1,12 @@
 #include "tri.h"
 
+void swap(T_liste a, T_liste b) {
+    struct T_cell *suiv_b = b->suiv;
+    struct T_cell *prec_b = b->prec;
+
+    b->prec = a->prec;
+    b->suiv = a->suiv;
+
+    a->prec = prec_b;
+    a->suiv = suiv_b;
+}
